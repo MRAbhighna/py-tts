86f3c305595db7bfb8101c2e063616143b43c3e4
MRAbhighna <bbsf.personal@gmail.com>
2025-08-08 18:25:16 -0400

    ADDED 'pyttx3' to master

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
-import pyttsx3 as ts # type: ignore (For Visual Studio)
+import pyttsx3 as ts
 
 e = ts.init()
 
 e.setProperty('rate', 150)
 e.setProperty('volume', 1)
 
-vI = int(input("Enter voice ID (Enter for DEFUALT): "))
+vI = input("Enter voice ID (Enter for DEFAULT): ")
+
+if vI != '':
+    vI = int(vI)
+else:
+    vI = 0
 
 v = e.getProperty('voices')
-e.setProperty('voice', v[vI].id)
+e.setProperty('voice', v[vI].id) # type: ignore (For Visual Studio)
 
 t = input("Enter text to speak: ")
 
